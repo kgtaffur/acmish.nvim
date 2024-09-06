f7b9a293e41c5c985c1562aa3088a8ca926e5d72
kg <86487929+kgtaffur@users.noreply.github.com>
2024-09-06 13:36:32 -0500

    lualine colors

--- lua/lualine/themes/rose-pine.lua
+++ lua/lualine/themes/rose-pine.lua
@@ -2,33 +2,36 @@ local p = require("rose-pine.palette")
 
 return {
 	normal = {
+		-- This is the label
 		a = { bg = "#8888cc", fg = p.base, gui = "bold" },
-		b = { bg = "#eaffff", fg = p.rose },
+		-- This one is the sides color
+		b = { bg = "#eaffff", fg = p.text },
+		-- This one is the middle
 		c = { bg = "#eaffff", fg = p.text },
 	},
 	insert = {
 		a = { bg = "#000099", fg = p.base, gui = "bold" },
-		b = { bg = "#eaffff", fg = p.foam },
+		b = { bg = "#eaffff", fg = p.text },
 		c = { bg = "#eaffff", fg = p.text },
 	},
 	visual = {
 		a = { bg = p.iris, fg = p.base, gui = "bold" },
-		b = { bg = "#eaffff", fg = p.iris },
+		b = { bg = "#eaffff", fg = p.text },
 		c = { bg = "#eaffff", fg = p.text },
 	},
 	replace = {
 		a = { bg = p.pine, fg = p.base, gui = "bold" },
-		b = { bg = "#eaffff", fg = p.pine },
+		b = { bg = "#eaffff", fg = p.text },
 		c = { bg = "#eaffff", fg = p.text },
 	},
 	command = {
 		a = { bg = p.love, fg = p.base, gui = "bold" },
-		b = { bg = "#eaffff", fg = p.love },
+		b = { bg = "#eaffff", fg = p.text },
 		c = { bg = "#eaffff", fg = p.text },
 	},
 	inactive = {
 		a = { bg = "#eaffff", fg = p.muted, gui = "bold" },
-		b = { bg = "#eaffff", fg = p.muted },
-		c = { bg = "#eaffff", fg = p.muted },
+		b = { bg = "#eaffff", fg = p.text },
+		c = { bg = "#eaffff", fg = p.text },
 	},
 }
